5bc8edcd0846693fc0381b536d4fae2cdd09f426
axelquartz <axelarep@gmail.com>
2026-08-13 20:14:17 -0600

    add values to the DOM but no localStorage

--- src/script.js
+++ src/script.js
@@ -1,1 +1,18 @@
-// alert("This is working!");
+let inputBtn = document.getElementById("input-btn");
+let inputDisplay = document.getElementById("save-el");
+let inputValue = document.getElementById("input-el");
+let myLeads = [];
+
+inputBtn.addEventListener("click", () => {
+  myLeads.push(inputValue.value);
+  inputDisplay.innerHTML += `<a href="${inputValue.value}" target="_blank">${inputValue.value}</a> `;
+  inputValue.value = "";
+
+  //   myLeads.push(inputValue.value);
+
+  //   for (let i = 0; i < myLeads.length; i++) {
+  //     console.log(myLeads[i]);
+  //     inputValue.value = "";
+  //     inputDisplay.textContent += myLeads[i] + " ";
+  //   }
+});
